423d60decc66830cdc6cb5f7891d342c28b9df12
unknown <okaforstanley147@gmail.com>
2025-11-08 22:36:26 -0800

    Library Tracker App

---
+++ Library Tracker App/LibraryTrackerApp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LibraryTrackerApp {
+public static void main (String [] args){
+
+Scanner input = new Scanner(System.in);
+String books = "The subtle art of not giving a fuck;Rich dad Poor dad;";
+
+        while (true) {
+            System.out.println("\n>>>>>>> Library Tracker App <<<<<<<");
+            System.out.println("1. View all books");
+            System.out.println("2. Borrow a book");
+            System.out.println("3. Return a book");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option (1-4): ");
+            int choice = input.nextInt();
+            input.nextLine();
+}
+
+
+
+
+
+
+
+
+
+
+}
+}
