461df1606933775d0ffee042ff0cc6a3aa792be4
unknown <okaforstanley147@gmail.com>
2025-11-09 01:22:01 -0800

    Library Tracker App

--- Library Tracker App/LibraryTrackerApp.java
+++ Library Tracker App/LibraryTrackerApp.java
@@ -4,7 +4,7 @@ public class LibraryTrackerApp {
 public static void main (String [] args){
 
 Scanner input = new Scanner(System.in);
-String books = "The subtle art of not giving a fuck;Rich dad Poor dad;";
+String books = "The subtle art of not giving a fuck-available;Rich dad Poor dad-available;";
 
         while (true) {
             System.out.println("\n>>>>>>> Library Tracker App <<<<<<<");
@@ -15,6 +15,37 @@ String books = "The subtle art of not giving a fuck;Rich dad Poor dad;";
             System.out.print("Choose an option (1-4): ");
             int choice = input.nextInt();
             input.nextLine();
+
+       switch (choice) {
+                case 1:
+                  
+                    if (books.isEmpty()) {
+                        System.out.println("No books available in the library.");
+                    } else {
+                        System.out.println("Books in the Library:");
+                        String[] bookList = books.split(";");
+                        for (int index = 0; index < bookList.length; index++) {
+                            if (!bookList[index].isEmpty()) {
+                                String[] parts = bookList[index].split("-");
+                                System.out.println((index + 1) + ". " + parts[0] + " (" + parts[1] + ")");
+                            }
+                        }
+                    }
+                    break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
 
 
